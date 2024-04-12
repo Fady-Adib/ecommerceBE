49e253b485f07b1741316cbf4a4d3b162a5386c7
Fady-Atef-Adeeb <fady.atef.adeeb@gmail.com>
2024-04-12 20:36:09 +0200

    first commit

--- db/db.connection.js
+++ db/db.connection.js
@@ -10,6 +10,7 @@ export const dbConnection = async () => {
       console.log(`db connected in ${process.env.DB_URL_ONLINE}`);
     })
     .catch((err) => {
+      console.log(process.env.DB_URL_ONLINE);
       console.log({ msg: "fail connect to db", err });
     });
 };
