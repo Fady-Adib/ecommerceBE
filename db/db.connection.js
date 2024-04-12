1fce6147b6e8ab758992b35f8ff66a60e3ae40b9
Fady-Atef-Adeeb <fady.atef.adeeb@gmail.com>
2024-04-12 19:59:32 +0200

    first commit

--- db/db.connection.js
+++ db/db.connection.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
   await mongoose
-    .connect(process.env.DB_URL_ONLINE)
+    .connect(process.env.DB_URL_ONLINE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
       console.log(`db connected in ${process.env.DB_URL_ONLINE}`);
     })
